b8992091742c16bed186cbc7841aec4a99dde4cb
Jeedan <Jeedan.Cuneyt@gmail.com>
2026-03-25 21:27:50 +0300

    added Jsonwebtokens. added makeJWT function to create a signed Token. added verifyJWT to verify and decode a token and return the userID. Wrote tests using Vitest to make sure creating and validating JWT works as expected.

---
+++ src/auth.test.ts
@@ -0,0 +1,41 @@
+import { describe, it, expect, beforeAll } from "vitest";
+import { checkPasswordHash, hashPassword, makeJWT, validateJWT } from "./auth";
+
+describe("Password Hashing", () => {
+	const password1 = "correctPassword123!";
+	const password2 = "anotherPassword456!";
+	let hash1: string;
+	let hash2: string;
+
+	beforeAll(async () => {
+		hash1 = await hashPassword(password1);
+		hash2 = await hashPassword(password2);
+	});
+
+	it("should return true for the correct password", async () => {
+		const result = await checkPasswordHash(password1, hash1);
+		expect(result).toBe(true);
+	});
+});
+
+describe("JWT", () => {
+	const secret = "123JWT";
+	const userID = "b15e9dec-246f-44d6-86db-100cb8bc19c8";
+	const signedToken = makeJWT(userID, 60, secret);
+	const decodeJWT = validateJWT(signedToken, secret);
+
+	it("should return true for a correctly signed JWT", async () => {
+		expect(decodeJWT === userID).toBe(true);
+	});
+
+	it("should throw for a wrong secret JWT", async () => {
+		expect(() => validateJWT(signedToken, "wrong_secret")).toThrow();
+	});
+
+	it("should throw for an expired JWT", async () => {
+		expect(() => {
+			const expiredToken = makeJWT(userID, 0, secret);
+			validateJWT(expiredToken, secret);
+		}).toThrow();
+	});
+});
--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,6 @@
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
 
 export async function hashPassword(password: string): Promise<string> {
 	const hash = await argon2.hash(password);
@@ -12,4 +14,44 @@ export async function checkPasswordHash(
 	const match = await argon2.verify(hash, password);
 	if (!match) return false;
 	return true;
+}
+
+// iss is the issuer of the token. Set this to chirpy
+// sub is the subject of the token, which is the user's ID.
+// iat is the time the token was issued. Use Math.floor(Date.now() / 1000) to get the current time in seconds.
+// exp is the time the token expires. Use iat + expiresIn to set the expiration
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+export function makeJWT(
+	userID: string,
+	expiresIn: number,
+	secret: string,
+): string {
+	const issuedAt = Math.floor(Date.now() / 1000);
+	const payload: payload = {
+		iss: "chirpy",
+		sub: userID,
+		iat: issuedAt,
+		exp: issuedAt + expiresIn,
+	};
+	const signed = jwt.sign(payload, secret);
+	return signed;
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+	try {
+		const token = jwt.verify(tokenString, secret) as JwtPayload;
+		const userID = token.sub;
+		if (!userID) throw new Error("No userID in token");
+		console.log(userID);
+		return userID;
+	} catch (err: unknown) {
+		if (err instanceof Error) {
+			console.error(err.message);
+			throw new Error(err.message);
+		} else {
+			console.error(err);
+			throw new Error(`Could not validate JWT: ${err}`);
+		}
+	}
 }
